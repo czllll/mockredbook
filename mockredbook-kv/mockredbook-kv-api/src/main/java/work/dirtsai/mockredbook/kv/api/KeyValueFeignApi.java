77ad07e0e32e0fe2435676230bc89cb0107b0c05
czllll <czl010106@163.com>
2025-02-19 14:28:15 +0000

    feat: 1.添加 Feign 客户端接口

--- mockredbook-kv/mockredbook-kv-api/src/main/java/work/dirtsai/mockredbook/kv/api/KeyValueFeignApi.java
+++ mockredbook-kv/mockredbook-kv-api/src/main/java/work/dirtsai/mockredbook/kv/api/KeyValueFeignApi.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import work.dirtsai.framework.common.response.Response;
 import work.dirtsai.mockredbook.kv.constant.ApiConstants;
 import work.dirtsai.mockredbook.kv.dto.req.AddNoteContentReqDTO;
+import work.dirtsai.mockredbook.kv.dto.req.FindNoteContentReqDTO;
+import work.dirtsai.mockredbook.kv.dto.resp.FindNoteContentRespDTO;
 
 @FeignClient(name = ApiConstants.SERVICE_NAME)
 public interface KeyValueFeignApi {
@@ -14,5 +16,9 @@ public interface KeyValueFeignApi {
 
     @PostMapping(value = PREFIX + "/note/content/add")
     Response<?> addNoteContent(@RequestBody AddNoteContentReqDTO addNoteContentReqDTO);
+
+
+    @PostMapping(value = PREFIX + "/note/content/find")
+    Response<FindNoteContentRespDTO> findNoteContent(@RequestBody FindNoteContentReqDTO findNoteContentReqDTO);
 
 }
