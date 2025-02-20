1ed62f3457a6b50013f86544211872c619fc7c9a
czllll <czl010106@163.com>
2025-02-20 22:16:12 +0000

    chore：uptodate

---
+++ mockredbook-note/mockredbook-note-biz/src/main/java/work/dirtsai/mockredbook/note/biz/domain/mapper/NoteDOMapper.java
@@ -0,0 +1,17 @@
+package work.dirtsai.mockredbook.note.biz.domain.mapper;
+
+import work.dirtsai.mockredbook.note.biz.domain.dataobject.NoteDO;
+
+public interface NoteDOMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(NoteDO record);
+
+    int insertSelective(NoteDO record);
+
+    NoteDO selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(NoteDO record);
+
+    int updateByPrimaryKey(NoteDO record);
+}
