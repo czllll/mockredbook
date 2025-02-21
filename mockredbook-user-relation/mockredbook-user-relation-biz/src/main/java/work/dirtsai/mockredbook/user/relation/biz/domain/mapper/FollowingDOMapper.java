db33d6d7b4753d74508e5c8c5b3b936291eabcd4
czllll <czl010106@163.com>
2025-02-21 17:26:57 +0000

    feat: 新增-关注用户接口

---
+++ mockredbook-user-relation/mockredbook-user-relation-biz/src/main/java/work/dirtsai/mockredbook/user/relation/biz/domain/mapper/FollowingDOMapper.java
@@ -0,0 +1,21 @@
+package work.dirtsai.mockredbook.user.relation.biz.domain.mapper;
+
+import work.dirtsai.mockredbook.user.relation.biz.domain.dataobject.FollowingDO;
+
+import java.util.List;
+
+public interface FollowingDOMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(FollowingDO record);
+
+    int insertSelective(FollowingDO record);
+
+    FollowingDO selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(FollowingDO record);
+
+    int updateByPrimaryKey(FollowingDO record);
+
+    List<FollowingDO> selectByUserId(Long userId);
+}
