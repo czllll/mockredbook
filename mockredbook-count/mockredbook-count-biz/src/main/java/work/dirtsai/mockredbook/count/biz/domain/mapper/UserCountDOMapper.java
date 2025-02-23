f34461b3a9b8ac71dc15470d9f2c73c8a19df258
czllll <czl010106@163.com>
2025-02-23 12:17:22 +0000

    feat: 新增-粉丝数写入redis和db；

--- mockredbook-count/mockredbook-count-biz/src/main/java/work/dirtsai/mockredbook/count/biz/domain/mapper/UserCountDOMapper.java
+++ mockredbook-count/mockredbook-count-biz/src/main/java/work/dirtsai/mockredbook/count/biz/domain/mapper/UserCountDOMapper.java
@@ -1,6 +1,7 @@
 package work.dirtsai.mockredbook.count.biz.domain.mapper;
 
 
+import org.apache.ibatis.annotations.Param;
 import work.dirtsai.mockredbook.count.biz.domain.dataobject.UserCountDO;
 
 public interface UserCountDOMapper {
@@ -15,4 +16,13 @@ public interface UserCountDOMapper {
     int updateByPrimaryKeySelective(UserCountDO record);
 
     int updateByPrimaryKey(UserCountDO record);
+
+    /**
+     * 添加或更新粉丝总数
+     * @param count
+     * @param userId
+     * @return
+     */
+    int insertOrUpdateFansTotalByUserId(@Param("count") Integer count, @Param("userId") Long userId);
+
 }
